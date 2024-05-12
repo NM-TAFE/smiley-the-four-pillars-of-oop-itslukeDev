d2980df981ab007051bd7e24ab343eebec225e9b
lukedev <82327125+itslukeDev@users.noreply.github.com>
2024-05-13 00:44:19 +0800

    refactor: auto lint

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """"Demonstrates the use of the Smiley class and its subclasses.
 If you have access to a SenseHAT (either via a Raspberry Pi or a SenseHAT emulator), you can use the real SenseHAT class instead of the mock SenseHAT class.
 That is, delete the sense_hat.py file that is included in this bundle."""
+
 import time
 
 from happy import Happy
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This is only needed if you have not deleted sense_hat.py
     # and only in some environments - uncomment only if you have errors
     # from multiprocessing import freeze_support
